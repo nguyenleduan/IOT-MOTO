2086a29449f5fb2764a5e32f4fb2063932a0270b
Nguyen Le Duan <nguyenleduan.index@gmail.com>
2022-10-13 08:39:59 +0700

    update

--- modul/modul.ino
+++ modul/modul.ino
@@ -3,13 +3,13 @@
 
 SoftwareSerial BTSerial(2, 3); // RX | TX  --->TX  | RX (HC-05)
 
-OneButton button (4, true);
-#define ledPin 7
-int state = 0;  
-int  pinPower = 5;
-int  pinCoi = 6;
-int  pinStartUp = 7;
-int  pinDen = 8;
+OneButton button (9, true);
+//#define ledPin 7
+int state = 0;
+int  pinPower = 4;
+int  pinCoi = 5;
+int  pinStartUp = 6;
+int  pinDen = 7;
 int power = 0;
 int countConnect = 0;
 unsigned long hientai = 0;
@@ -19,77 +19,97 @@ int timecho = 5000;
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("Enter AT commands:"); 
-  BTSerial.begin(9600); 
-  button.attachClick(startPower); 
-  button.attachLongPressStart(lockPower);   
+  Serial.println("Enter AT commands:");
+  BTSerial.begin(9600);
+  button.attachClick(startPower);
+  button.attachLongPressStart(lockPower);
   pinMode(pinCoi, OUTPUT);
   pinMode(pinPower, OUTPUT);
   pinMode(pinDen, OUTPUT);
-  pinMode(pinStartUp, OUTPUT); 
-  digitalWrite(pinCoi, LOW);
-  digitalWrite(pinPower, LOW);
-  digitalWrite(pinDen, LOW);
-  digitalWrite(pinStartUp, LOW);
+  pinMode(pinStartUp, OUTPUT);
+  digitalWrite(pinCoi, HIGH);
+  digitalWrite(pinPower, HIGH);
+  digitalWrite(pinDen, HIGH);
+  digitalWrite(pinStartUp, HIGH);
 }
 void loop()
-{ 
+{
   delayMililis();
   button.tick();
-  if (BTSerial.available()){ 
-//    Serial.print("\n Data:"); // doc data tu app
-//     Serial.write(BTSerial.read());
+  if (BTSerial.available()) {
+    //    Serial.print("\n Data:"); // doc data tu app
+    //     Serial.write(BTSerial.read());
     int a =  BTSerial.read();
-    countConnect = 0; 
+    countConnect = 0;
     checkData(a);
-  }else{  
-    if(countConnect == 60){
+  } else {
+    if (countConnect == 60) {
       lockPower();
     }
-  } 
-  if (Serial.available()){ // AT
-    BTSerial.write(Serial.read()); 
+  }
+  if (Serial.available()) { // AT
+    BTSerial.write(Serial.read());
   }
 }
 
-void delayMililis(){
+void delayMililis() {
   thoigian = millis();
-  if(thoigian - hientai >= timecho){
-    hientai = millis(); 
+  if (thoigian - hientai >= timecho) {
+    hientai = millis();
     Serial.print("\nTime delayy :");
     Serial.print(countConnect);
-    if(countConnect >= 10){
+    if (countConnect >= 10) {
       // off all
-    Serial.print("\n off all");
-    }else{ 
-    countConnect = countConnect +1; 
+      Serial.print("\n off all");
+    } else {
+      countConnect = countConnect + 1;
     }
   }
 }
-void checkData(int  data){
-   
-   switch (data) {
- case 49:
-    Serial.print("\n Power on");
-    break;
-  case 50:
-    Serial.print("\n Start up");
-    break;
-  case 51:
-    Serial.print("\n Coi xe");
-    break; 
-  case 120:
-  countConnect = 0;
-    Serial.print("\n smart phone runing");
-    break; 
+void checkData(int  data) {
+
+  switch (data) {
+    case 49:
+      if (digitalRead(pinPower) == 1) {
+        digitalWrite(pinPower, LOW);
+      } else {
+        digitalWrite(pinPower, HIGH);
+      }
+      Serial.print("\n Power on");
+      break;
+    case 50: // de 2s
+      Serial.print("\n Start up");
+      digitalWrite(pinPower, HIGH);
+      digitalWrite(pinPower, LOW);
+      delay(2000);
+      digitalWrite(pinPower, HIGH);
+      break;
+    case 51:
+      Serial.print("\n Coi xe");
+      digitalWrite(pinPower, LOW);
+      delay(500);
+      digitalWrite(pinPower, HIGH);
+      break;
+    case 120:
+      countConnect = 0;
+      Serial.print("\n smart phone runing");
+      break;
   }
 }
-void startPower(){
-  
-} 
-void lockPower(){
-  power = 0; 
-  
+void startPower() {
+  if (countConnect < 10) {
+    if (digitalRead(pinPower) == 1) {
+      digitalWrite(pinPower, LOW);
+    } else {
+      digitalWrite(pinPower, HIGH);
+    }
+  } else { 
+    digitalWrite(pinPower, HIGH);
+  }
+
+}
+void lockPower() {
+  power = 0;
   digitalWrite(pinCoi, LOW);
   digitalWrite(pinPower, LOW);
   digitalWrite(pinDen, LOW);
